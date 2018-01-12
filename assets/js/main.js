d454e1f8984cc17a58eaac0845606f80092cd825
Prilivia Ruli Fatmawati <prilivia98@gmail.com>
2018-01-13 06:10:05 +0700

    adding nicescroll, adding news-list.html and blog.html

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,4 +17,22 @@ $(document).ready(function(){
 
    // lightcase
 	 $('a[data-rel^=lightcase]').lightcase();
+
+   // nicescroll
+	 $("body").niceScroll({
+		 cursorcolor:"#95a5a6",
+		 cursorwidth:"5px",
+		 cursorborder:"1px solid transparent",
+		 cursorborderradius:"0px"
+	 });
+
+	 // to ease when click '#' url
+	$(document).on('click', 'a', function(event){
+	    // event.preventDefault();
+
+	    $('html, body').animate({
+	        scrollTop: $( $.attr(this, 'href') ).offset().top
+	    }, 1000);
+	});
+
  });
